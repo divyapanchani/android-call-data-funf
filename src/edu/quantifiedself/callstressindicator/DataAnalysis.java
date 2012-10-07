1d6d8991a0cecf77c8e33175c0a0bead52bd7d54
admin@gkomninos.com <admin@gkomninos.com@e26fd566-265c-e360-bbd9-01ec37e8beef>
2012-10-07 12:02:01 +0000

    calculating score foreach call

--- src/edu/quantifiedself/callstressindicator/DataAnalysis.java
+++ src/edu/quantifiedself/callstressindicator/DataAnalysis.java
@@ -1,46 +1,53 @@
 package edu.quantifiedself.callstressindicator;
 
-import java.util.ArrayList;
-
 public class DataAnalysis {
 	
-	public static double rmsAvg(double [] data){
-	int size = data.length;
-	double sum = 0;
-	for(int i=0;i<size;i++)
-		sum = sum + data[size];
-	return sum / size;
+    public static double rms(double[] nums){
+        double ms = 0;
+        for (int i = 0; i < nums.length; i++)
+            ms += nums[i] * nums[i];
+        ms /= nums.length;
+        return Math.sqrt(ms);
+    }
+	
+	public static double updateValues(double newValue, double oldValue){
+		return (0.6*oldValue + 0.4*newValue);
 	}
 	
-	public static int getScore(double [] data, double threshold){
+	/*
+	 * returns the loudness score for the call between 1 - 10
+	 */
+	public static int getScaledScore(double [] data, double min, double max, double avg){
 		int N = data.length;
-		int over = 0;
+		int total_score = 0;
 		for(int i=0;i<N;i++){
-			if(data[i] > threshold)
-				over++;
+			total_score += getScore(data[i], min, avg, max);
 		}
-		return (10 * over) / N;
+		return (10 * total_score) / N;
+	}
+	
+	/*
+	 * returns a score based on min, max, avg values
+	 */
+	public static int getScore(double rmsValue, double min, double avg, double max){
+		double lower_bound = min +min*5/100;
+		double upper_bound = max - max*5/100;
+		double lower_avg = avg - avg*5/100;
+		double upper_avg = avg + avg*5/100;
+		if(lower_bound < rmsValue && rmsValue < lower_avg)
+			return 0;
+		if(lower_avg < rmsValue && rmsValue < upper_avg)
+			return 0;
+		if(upper_avg < rmsValue && rmsValue < upper_bound)
+			return 1;
+		return 0;
 	}
 
-	public static ArrayList<Double> overThreshold(double [] data, double threshold){
-		ArrayList<Double> a = new ArrayList<Double>();
-		int cons = 0;
-		for(int i=0;i<data.length;i++){
-			if(data[i] > threshold)
-				cons++;
-			else
-				cons = 0;
-			if(cons == 3)
-				a.add(data[i]);
-		}
-		return a;
-	}
-	
 	public static double [] minMaxAvg(double [] data){
 		int size = data.length;
 		double sum = 0;
 		double min = 100.0;
-		double max = 100.0;
+		double max = -1.0;
 		for(int i=0;i<size;i++){
 			sum = sum + data[i];
 			if(data[i] > max)
